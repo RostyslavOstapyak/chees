f1046ffe573a0e37dae558fbe006f376951d20e5
RostyslavOstapyak <rostyslav.ostapyak@gmail.com>
2022-05-16 18:46:17 +0300

    implement: vertical movement

--- src/models/figures/Queen.ts
+++ src/models/figures/Queen.ts
@@ -4,11 +4,18 @@ import {Cell} from "../Cell";
 import blackLogo from "../../assets/black-queen.png";
 import whiteLogo from "../../assets/white-queen.png";
 
-export class Queen extends Figure{
+export class Queen extends Figure {
 
     constructor(color: Colors, cell: Cell) {
         super(color, cell);
         this.logo = color === Colors.BLACK ? blackLogo : whiteLogo;
         this.name = FigureNames.QUEEN;
+    }
+
+    canMove(target: Cell): boolean {
+        if (!super.canMove(target)) return false;
+        if (this.cell.isEmptyVertical(target)) return true;
+
+        return false
     }
 }
--- src/models/figures/Rook.ts
+++ src/models/figures/Rook.ts
@@ -4,11 +4,17 @@ import {Cell} from "../Cell";
 import blackLogo from "../../assets/black-rook.png";
 import whiteLogo from "../../assets/white-rook.png";
 
-export class Rook extends Figure{
+export class Rook extends Figure {
 
     constructor(color: Colors, cell: Cell) {
         super(color, cell);
         this.logo = color === Colors.BLACK ? blackLogo : whiteLogo;
         this.name = FigureNames.ROOK;
+    }
+
+    canMove(target: Cell): boolean {
+        if (!super.canMove(target)) return false;
+
+        return true
     }
 }
