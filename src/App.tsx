c21d8263ab8dd370c9af425765368897b5ffc5a8
RostyslavOstapyak <rostyslav.ostapyak@gmail.com>
2022-05-17 13:40:52 +0300

    implement:move swap

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,23 @@ import React, {useEffect, useState} from 'react';
 import './App.css';
 import BoardComponent from "./components/BoardComponent";
 import {Board} from "./models/Board";
+import {Player} from "./models/Player";
+import {Colors} from "./models/Colors";
 
 function App() {
     const [board, setBoard] = useState(new Board());
+    const [whitePlayer, setWhitePlayer] = useState(new Player(Colors.WHITE));
+    const [blackPlayer, setBlackPlayer] = useState(new Player(Colors.BLACK));
+    const [currentPlayer, setCurrentPlayer] = useState<Player | null>(null);
 
     useEffect(() => {
         restart();
-    }, [])
+        setCurrentPlayer(whitePlayer);
+    }, []);
+
+    function swapPlayer() {
+        setCurrentPlayer(currentPlayer?.color === Colors.WHITE ? blackPlayer : whitePlayer)
+    }
 
     const restart = () => {
         const newBoard = new Board();
@@ -21,6 +31,8 @@ function App() {
             <BoardComponent
                 board={board}
                 setBoard={setBoard}
+                currentPlayer={currentPlayer}
+                swapPlayer={swapPlayer}
             />
         </div>
     );
