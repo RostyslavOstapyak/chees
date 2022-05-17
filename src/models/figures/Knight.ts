764521a7487bf3d66dbcd0207288225a4e2b9724
RostyslavOstapyak <rostyslav.ostapyak@gmail.com>
2022-05-17 13:14:23 +0300

    implement: knight move

--- src/models/figures/Knight.ts
+++ src/models/figures/Knight.ts
@@ -4,7 +4,7 @@ import {Cell} from "../Cell";
 import blackLogo from "../../assets/black-knight.png";
 import whiteLogo from "../../assets/white-knight.png";
 
-export class Knight extends Figure{
+export class Knight extends Figure {
 
     constructor(color: Colors, cell: Cell) {
         super(color, cell);
@@ -14,7 +14,10 @@ export class Knight extends Figure{
 
     canMove(target: Cell): boolean {
         if (!super.canMove(target)) return false;
+        // dx -> delta X
+        const dx = Math.abs(this.cell.x - target.x);
+        const dy = Math.abs(this.cell.y - target.y);
 
-        return true
+        return (dx === 1 && dy === 2) || (dx === 2 && dy === 1)
     }
 }
